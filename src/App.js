e96852d8b9fdb9cd38176ad7b2ecb61e4e55818e
Weronika Kansy <weronika.kansy@gmail.com>
2022-04-03 23:25:55 +0200

    Added the Die component and rendered it inside a new container in main; dice laid out with grid, other styles added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Die from "./components/Die";
+
 function App() {
   return (
     <div className="container">
-      <main className="main"></main>
+      <main className="main">
+        <div className="dice-container">
+          <Die value="1" />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+export default function Die({ value }) {
+  return (
+    <div className="die">
+      <h2 className="die__number">{value}</h2>
+    </div>
+  );
+}
