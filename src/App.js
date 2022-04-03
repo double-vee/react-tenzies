e6d2329735d0f730f3953b1cf34b11e8f925f678
Weronika Kansy <weronika.kansy@gmail.com>
2022-04-04 00:40:35 +0200

    Initialized the dice state by calling a function that returns an array of random numbers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState(() => allNewDice());
+
+  function allNewDice() {
+    const initialArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    return initialArray.map((number) => Math.floor(Math.random() * 6 + 1));
+  }
+
+  console.log(dice);
+
   return (
     <div className="container">
       <main className="main">
